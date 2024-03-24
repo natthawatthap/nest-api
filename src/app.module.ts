00487b99494a9b272b0c2c90e5feaa4e77a0d7d7
natthawatthap <natthawatthap@gmail.com>
2024-03-24 17:13:26 +0700

    add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, PrismaService],
+  controllers: [UsersController],
+  providers: [UsersService, PrismaService],
 })
 export class AppModule {}
